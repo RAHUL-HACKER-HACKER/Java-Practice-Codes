e7087d9b5fb7aeca39be3cc5947f0c9437be50cf
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-09-10 16:37:48 +0530

    arr.add(" a");

--- Array.java
+++ Array.java
@@ -8,8 +8,8 @@ public class Array {
 		
 		ArrayList<String> arr=new ArrayList<String>();
 	  
+		arr.add("rahul");
 		
-		arr.add(" a");
 		arr.add(" best");
 		arr.add(" engineer");
 		System.out.println(arr);//1st execution
